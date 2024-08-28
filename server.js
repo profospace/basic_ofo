5ff510e368fb99961dc36f293b7732a425132da0
ihaveafix <146871885+30under30lost@users.noreply.github.com>
2024-08-28 06:09:04 +0530

    removed analytics

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const amplitude = require('@amplitude/analytics-node');
-const axios = require('axios');
 bodyParser = require('body-parser');
 const uuid = require('uuid'); // Import the uuid library
 const AWS = require('aws-sdk');
