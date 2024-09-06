96892da59314db8432c8d29e3c2fa5e65e0cab70
adit chauhan <adit@classplus.co>
2024-09-06 17:42:27 +0530

    added type in the ads

--- .history/server_20240902172839.js
+++ .history/server_20240902172839.js
@@ -6,7 +6,7 @@ const fileUpload = require('express-fileupload');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3018;
+const PORT = process.env.PORT || 3011;
 
 app.use(fileUpload());
 app.use(cors());
