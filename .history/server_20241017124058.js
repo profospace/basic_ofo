f887ec3bda629d294a2f815d5f753b449166886d
adit chauhan <adit@classplus.co>
2024-10-24 13:33:18 +0530

    upload carousel item

--- .history/server_20241017124058.js
+++ .history/server_20241017124058.js
@@ -6,7 +6,7 @@ const fileUpload = require('express-fileupload');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 2041;
+const PORT = process.env.PORT || 2039;
 
 app.use(fileUpload());
 app.use(cors());
